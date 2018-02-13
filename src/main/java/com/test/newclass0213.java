6a84d0d5986db243b58c0a4f691906450f576e84
chenchi <18758125177@163.com>
2018-02-13 11:29:13 +0800

    newclass0213

---
+++ src/main/java/com/test/newclass0213.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by wb-chenchi on 2018/2/13.
+ */
+public class newclass0213 {
+    public static void main(String[] args){
+        System.out.print("123");
+    }
+}
